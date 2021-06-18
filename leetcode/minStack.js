003b6eb9696ffc7200f1c0098d74419d2d487a2c
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2021-06-18 12:21:36 +0800

    add minStack.js

---
+++ leetcode/minStack.js
@@ -0,0 +1,57 @@
+// 最小栈
+const minStack = function () {
+    this.items = [];
+    this.min = null;
+}
+
+minStack.prototype = {
+    push(val){
+        if (!this.items.length) this.min = val;
+        this.min = Math.min(val,this.min);
+        this.items.push(val)
+    },
+    pop(){
+        if(!this.items.length) return ;
+        let num = this.items.pop();
+        this.min = Math.min(...this.items)
+        return num;
+    },
+    top(){
+        if(!this.items.length) return null;
+        return this.items[this.items.length-1];
+    },
+    getMin(){
+        return this.min;
+    }
+}
+// 进栈
+// minStack.prototype.push = function (val) {
+//     if (!this.items.length) this.min = val;
+//     this.min = Math.min(val,this.min);
+//     this.items.push(val)
+// }
+
+// //出栈
+// minStack.prototype.pop = function () {
+//     if(!this.items.length) return ;
+//     let num = this.items.pop();
+//     this.min = Math.min(...this.items)
+//     return num;
+// }
+
+// //获取栈顶元素
+// minStack.prototype.top = function () {
+//     if(!this.items.length) return null;
+//     return this.items[this.items.length-1];
+// }
+
+// minStack.prototype.getMin = function () {
+//     return this.min;
+// }
+
+let arr = new minStack();
+arr.push(5)
+arr.push(10)
+arr.push(15)
+arr.pop()
+console.log(arr.top())
